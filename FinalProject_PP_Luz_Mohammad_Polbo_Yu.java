2cfee41616b7ce95dafde1a918c15a2b66d8d6f1
Sederiel <sederiel0100@gmail.com>
2024-12-16 19:53:53 +0800

    Import scanner

--- FinalProject_PP_Luz_Mohammad_Polbo_Yu.java
+++ FinalProject_PP_Luz_Mohammad_Polbo_Yu.java
@@ -1,5 +1,7 @@
+import java.util.Scanner;
+
 public class FinalProject_PP_Luz_Mohammad_Polbo_Yu {
     public static void main(String[] args) {
-
+        Scanner s = new Scanner(System.in);
     }
 }
